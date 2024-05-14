6c836583adca0042ee619ffd549ce50aa0fc768a
Brian0-2 <sct115xd@hotmail.com>
2024-05-14 16:36:38 -0600

    Reading data from header form creating a state

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from "react";
+import { ChangeEvent, useEffect, useMemo, useState } from "react";
 import { NavLink, useLocation
         // Link 
 } from "react-router-dom"
@@ -6,16 +6,29 @@ import { useAppStore } from "../stores/useAppStore";
 
 export default function Header() {
 
+    const [searchFilters, setSearchFilters] = useState({
+        ingredient: '',
+        category: ''
+    });
+
     //useLocation es un hook especial de react-router-doom
     const {pathname} = useLocation();
 
     //Destectar la pagina de inicio retorna true o false 
     const isHome = useMemo(() => pathname === '/', [pathname]);
     const fethCategories = useAppStore((state) => state.fetchCategories);
-    
+    const {drinks} = useAppStore((state) => state.categories);
+
     useEffect(()=>{
         fethCategories()
     },[]);
+
+    const handleChange = (e:  ChangeEvent<HTMLInputElement>  | ChangeEvent<HTMLSelectElement>) => {
+        setSearchFilters({
+            ...searchFilters,
+            [e.target.name] : e.target.value
+        })
+    }
 
   return (
     <header className={isHome ? 'bg-header bg-center bg-cover' : 'bg-slate-800'}>
@@ -42,26 +55,38 @@ export default function Header() {
                             Nombre o Ingredientes
                         </label>
                         <input 
-                            id="ongredient"
+                            id="ingredient"
                             type="text" 
                             name="ingredient"
                             className="p-3 w-full rounded-lg focus:outline-none"
                             placeholder="Nombre o Ingrediente. Ej: Vodka, Tequila, Café"
+                            onChange={handleChange}
+                            value={searchFilters.ingredient}
                         />
                     </div>
                     <div className="space-y-4">
                         <label 
-                            htmlFor="ingredient"
+                            htmlFor="category"
                             className="block text-white uppercase font-extrabold text-lg"
                         >
                            Categoria
                         </label>
                         <select 
-                            id="ongredient"
-                            name="ingredient"
+                            id="category"
+                            name="category"
                             className="p-3 w-full rounded-lg focus:outline-none"
+                            onChange={handleChange}
+                            value={searchFilters.category}
                         >
                             <option value="">--Seleccione--</option>
+                            {drinks.map(drink => (
+                                <option 
+                                    key={drink.strCategory} 
+                                    value={drink.strCategory}
+                                >
+                                    {drink.strCategory}
+                                </option>
+                            ))}
                         </select>
                     </div>
                     <input type="submit" value="Buscar Recetas" className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase" />
